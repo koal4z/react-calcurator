14a8b86c2e0902301c1ce63061ecd9747dedc332
Koal4z <kkamkham@gmail.com>
2020-09-22 02:57:18 +0700

    css calculator

--- src/App.js
+++ src/App.js
@@ -8,18 +8,18 @@ function App() {
         <div id="display">display</div>
         <div id="clear">ac</div>
         <div id="add">+</div>
-        <div id="subtract">-</div>
-        <div id="multiply">x</div>
         <div id="divide">/</div>
-        <div id="nine">9</div>
-        <div id="eight">8</div>
+        <div id="multiply">x</div>
+        <div id="subtract">-</div>
         <div id="seven">7</div>
-        <div id="six">6</div>
-        <div id="five">5</div>
+        <div id="eight">8</div>
+        <div id="nine">9</div>
         <div id="four">4</div>
-        <div id="three">3</div>
+        <div id="five">5</div>
+        <div id="six">6</div>
         <div id="one">1</div>
         <div id="two">2</div>
+        <div id="three">3</div>
         <div id="zero">0</div>
         <div id="decimal">.</div>
         <div id="equals">=</div>
