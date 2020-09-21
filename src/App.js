4cdc249088b6c9e0ffb87b3cf1b5800068e208c1
Koal4z <kkamkham@gmail.com>
2020-09-22 06:14:06 +0700

    add click

--- src/App.js
+++ src/App.js
@@ -1,27 +1,97 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const operation = ['+', '-', 'x', '/'];
+  const [display, setDisplay] = useState('0');
+  useEffect(() => {
+    if (display.length > 1 && display[0] === '0') {
+      setDisplay(display.slice(1));
+    }
+    return;
+  }, [display]);
+
   return (
     <div className="App">
       <div className="calculator">
-        <div id="display">display</div>
-        <div id="clear">ac</div>
-        <div id="add">+</div>
-        <div id="divide">/</div>
-        <div id="multiply">x</div>
-        <div id="subtract">-</div>
-        <div id="seven">7</div>
-        <div id="eight">8</div>
-        <div id="nine">9</div>
-        <div id="four">4</div>
-        <div id="five">5</div>
-        <div id="six">6</div>
-        <div id="one">1</div>
-        <div id="two">2</div>
-        <div id="three">3</div>
-        <div id="zero">0</div>
-        <div id="decimal">.</div>
+        <div id="display">{display}</div>
+        <div id="clear" onClick={() => setDisplay('0')}>
+          ac
+        </div>
+        <div
+          id="add"
+          onClick={() => {
+            if (operation.indexOf(display[display.length - 1]) === -1)
+              setDisplay(display + '+');
+          }}
+        >
+          +
+        </div>
+        <div
+          id="divide"
+          onClick={() => {
+            if (operation.indexOf(display[display.length - 1]) === -1)
+              setDisplay(display + '/');
+          }}
+        >
+          /
+        </div>
+        <div
+          id="multiply"
+          onClick={() => {
+            if (operation.indexOf(display[display.length - 1]) === -1)
+              setDisplay(display + 'x');
+          }}
+        >
+          x
+        </div>
+        <div
+          id="subtract"
+          onClick={() => {
+            if (operation.indexOf(display[display.length - 1]) === -1)
+              setDisplay(display + '-');
+          }}
+        >
+          -
+        </div>
+        <div id="seven" onClick={() => setDisplay(display + '7')}>
+          7
+        </div>
+        <div id="eight" onClick={() => setDisplay(display + '8')}>
+          8
+        </div>
+        <div id="nine" onClick={() => setDisplay(display + '9')}>
+          9
+        </div>
+        <div id="four" onClick={() => setDisplay(display + '4')}>
+          4
+        </div>
+        <div id="five" onClick={() => setDisplay(display + '5')}>
+          5
+        </div>
+        <div id="six" onClick={() => setDisplay(display + '6')}>
+          6
+        </div>
+        <div id="one" onClick={() => setDisplay(display + '1')}>
+          1
+        </div>
+        <div id="two" onClick={() => setDisplay(display + '2')}>
+          2
+        </div>
+        <div id="three" onClick={() => setDisplay(display + '3')}>
+          3
+        </div>
+        <div
+          id="zero"
+          onClick={() => {
+            if (display !== '0') setDisplay(display + '0');
+          }}
+        >
+          0
+        </div>
+        <div id="decimal" onClick={() => setDisplay(display + '.')}>
+          .
+        </div>
         <div id="equals">=</div>
       </div>
     </div>
